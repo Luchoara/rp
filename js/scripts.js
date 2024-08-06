8b56924a3ba61ea75fa155cdee0d401709baa7aa
Lucho <cheluiviajes@gmail.com>
2024-08-06 13:26:22 -0300

    edited photo form medicare, styles a in nav-link

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,38 @@
-/*!
-* Start Bootstrap - Landing Page v6.0.6 (https://startbootstrap.com/theme/landing-page)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document
+	.getElementById("contact-form")
+	.addEventListener("submit", function (event) {
+		event.preventDefault(); // Evitar el envío predeterminado del formulario
+
+		// Aquí obtendrás los valores de los campos del formulario
+		const firstName = document.getElementById("firstName").value;
+		const lastName = document.getElementById("lastName").value;
+		const state = document.getElementById("state").value;
+		const zipCode = document.getElementById("zipCode").value;
+		const emailAddress = document.getElementById("emailAddress").value;
+		const phoneNumber = document.getElementById("phoneNumber").value;
+		const insuranceType = document.getElementById("insuranceType").value;
+
+		// Configuración del template params para EmailJS
+		const templateParams = {
+			firstName: firstName,
+			lastName: lastName,
+			state: state,
+			zipCode: zipCode,
+			emailAddress: emailAddress,
+			phoneNumber: phoneNumber,
+			insuranceType: insuranceType,
+		};
+
+		// Enviar email usando EmailJS
+		emailjs.send("service_5pf0e4k", "template_ru7s0pn", templateParams).then(
+			function (response) {
+				console.log("SUCCESS!", response.status, response.text);
+				alert("Sent!");
+				document.getElementById("contact-form").reset(); // Limpiar el formulario
+			},
+			function (error) {
+				console.log("FAILED...", error);
+				alert("Something wrong happend!");;
+			}
+		);
+	});
